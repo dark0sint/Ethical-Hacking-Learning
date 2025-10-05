461d1279667717dbd25661ead40c548ed0283ad3
DARK OSINT <levianawulandari7@gmail.com>
2025-10-05 18:53:34 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Dashboard from './pages/Dashboard';
+import Courses from './pages/Courses';
+import
